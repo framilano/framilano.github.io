a84e983218e53e28a18c6f68977cc3189b458731
fra98 <milano.francesco@protonmail.com>
2020-03-27 18:49:59 +0100

    Added food, vip, jobs

--- nomicosecitta/scripts/randomletter.js
+++ nomicosecitta/scripts/randomletter.js
@@ -8,22 +8,18 @@ function ranlet() {
 }
 
 function updatetotal() {
-    var total = 0
+    var total = 0.0
     points = document.getElementsByName('point')
     points.forEach(element => {
         total += parseFloat(element.value);
     });
-
-    document.getElementById('total').innerHTML = "Punteggio totale: " + total;
+    total = total.toFixed(1)
+    document.getElementById('total').innerHTML = "👑 Punteggio totale: " + total + " 👑";
 }
 
 function showtable() {
    document.getElementById('gametable').hidden=0
     
-}
-
-function showvalue(newelement, newvalue) {
-    newelement.innerHTML = newvalue
 }
 
 var counter = 0
@@ -33,7 +29,7 @@ function appendRow() {
     const newRow = document.createElement('tr')
     let td, input;
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 7; i++) {
         td = document.createElement('td')
         input = document.createElement('input')
         input.setAttribute('type', 'text')
@@ -50,14 +46,14 @@ function appendRow() {
     input.setAttribute('max', 4)
     input.setAttribute('step', 0.5)
     input.setAttribute('name', 'point')
-    input.setAttribute('onchange', 'updatetotal();showvalue(document.getElementById(\'' + counter + '\'),this.value)')
+    input.setAttribute('oninput', 'updatetotal();document.getElementById(\'' + counter + '\').innerHTML = parseFloat(this.value).toFixed(1)')
     input.defaultValue = 0
     td.appendChild(input)
-    newRow.appendChild(td)
-
-    td = document.createElement('td')
-    td.setAttribute('id', counter)
-    td.innerHTML = '0.0'
+    //div è dedicato al punteggio live ottenuto sotto un input type range
+    div = document.createElement('div')
+    div.setAttribute('id', counter)
+    div.innerHTML = 0.0
+    td.appendChild(div)
     newRow.appendChild(td)
 
     document.getElementById('gametable').appendChild(newRow);
