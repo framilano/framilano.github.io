3e712807aedd8ca2b88278b647413ffa5d2589b5
framilano <milano.francesco@protonmail.com>
2020-05-01 14:14:58 +0200

    First release of guesswho me

--- indovinachiME/scripts/engine.js
+++ indovinachiME/scripts/engine.js
@@ -1,11 +1,21 @@
+
 characters = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", 
-"13.png", "14.png", "15.png", "16.png", "17.png", "18.png", "19.png", "20.png", "21.png", "22.png", "23.png", "24.png", ]
+"13.png", "14.png", "15.png", "16.png", "17.png", "18.png", "19.png", "20.png", "21.png", "22.png", "23.png", "24.png"]
 
 score = 0
 
-
 function showrandomcharacter() {
-
+    if (characters.length <= 0) {
+        alert("Sono finiti i personaggi! Premi F5 per ricominciare")
+        return
+    }
+    image = document.getElementById('chosenone')
+    rdn_char = parseInt((Math.random() * 100) % characters.length)
+    image.setAttribute('src', "characters/"+characters[rdn_char])
+    characters.splice(rdn_char, 1)
+    image.setAttribute('width', '150')
+    image.setAttribute('style', "border: 10px solid red;border-radius:15px")
+    image.setAttribute('height', '168')
 }
 
 function setvolume() {
