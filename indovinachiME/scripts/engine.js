441619c1c4aafdb6d16e94de844f5f26e90ec42a
framilano <milano.francesco@protonmail.com>
2020-05-01 03:47:45 +0200

    Added first release of guesswhoME

---
+++ indovinachiME/scripts/engine.js
@@ -0,0 +1,56 @@
+characters = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", 
+"13.png", "14.png", "15.png", "16.png", "17.png", "18.png", "19.png", "20.png", "21.png", "22.png", "23.png", "24.png", ]
+
+score = 0
+
+
+function showrandomcharacter() {
+
+}
+
+function setvolume() {
+    var audio = document.getElementById("backgroundmusic");
+    audio.volume = 0.03;
+}
+
+function increase_score() {
+    score++;
+    document.getElementById('score').innerHTML = "Punteggio: "+ String(score)
+}
+
+function reset_game() {
+    img_list = document.getElementsByTagName('img')
+    for(i = 0; i < 24; i++) {
+        img_list[i].setAttribute('class', '')
+    }
+}
+
+function grey_out(image) {
+    classname = image.getAttribute('class')
+    if (classname == 'greyed_out') {
+        image.setAttribute('class', '')
+        return
+    }
+    image.setAttribute('class', 'greyed_out')
+}
+
+function generate_table() {
+    nuovatabella = document.getElementById('gametable')
+    for (i = 0; i < 4; i++) {
+        tr = document.createElement('tr')
+        z = i * 6
+        for (j = 0; j < 6; j++) {
+            td = document.createElement('td')
+            img = document.createElement('img')
+            img.setAttribute('src', "characters/" + characters[z])
+            img.setAttribute('id', z)
+            img.setAttribute('width', '150')
+            img.setAttribute('height', '168')
+            img.setAttribute('onclick', 'grey_out(document.getElementById('+ String(z) +'))')
+            z++
+            td.append(img)
+            tr.append(td)
+        }
+        nuovatabella.append(tr)
+    }
+}
