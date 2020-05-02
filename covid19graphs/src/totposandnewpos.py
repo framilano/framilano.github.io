53847ed13ff04f9ea6ccc8f8b8fe9876e922a35d
framilano <milano.francesco@protonmail.com>
2020-05-02 20:03:24 +0200

    Added new graph

--- covid19graphs/src/totposandnewpos.py
+++ covid19graphs/src/totposandnewpos.py
@@ -9,34 +9,43 @@ def removehour(element):
 
 
 def main():
-    dataframe = pd.read_csv(
-        "dpc-covid19-ita-andamento-nazionale.csv", sep=",", index_col=0)
+    dataframe = pd.read_csv("dpc-covid19-ita-andamento-nazionale.csv", sep=",", index_col=0)
+
     # Rimuovo gli orari, tengo la data
     fixeddate = []
     for h in dataframe.index:
         h = h.split('T')[0]
         fixeddate.append(h)
 
+    #Sostituisco l'indice con i nuovi dati
     dataframe.index = fixeddate
 
+    #tema nero
     plt.style.use('dark_background')
 
+    #genero la figura e l'array a due posizioni per i due plot
     figure, axes = plt.subplots(2, 1)
+    #x è sempre i giorni
     x = dataframe.index
     y = dataframe['totale_positivi']
     axes[0].set_title('Totale positivi', fontsize=20)
+    #axes[0] conterrà il primo grafico a barre
     axes[0].bar(x, y, color="blue")
 
     y = dataframe['variazione_totale_positivi']
     axes[1].set_title('Variazione positivi', fontsize=20)
+    #axes[0] conterrà il secondo grafico a barre
     axes[1].bar(x, y, color="red")
+
+    #Ruoto i label sull'asse delle x in verticale
     for tick in axes[0].get_xticklabels():
         tick.set_rotation(90)
 
     for tick in axes[1].get_xticklabels():
         tick.set_rotation(90)
 
-    figure.set_size_inches(21, 10)
+    #Modifico le dimensioni della figura e salvo la figura
+    figure.set_size_inches(19.2, 10.8)
     figure.tight_layout(pad=3.0)
     figure.savefig("../assets/totposandvariation.png", dpi=100)
 
