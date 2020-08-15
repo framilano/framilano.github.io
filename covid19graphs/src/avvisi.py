ad4b9345ae72e66834c1c3cb39ca4392a868213b
framilano <milano.francesco@protonmail.com>
2020-08-15 23:44:55 +0200

    Added avvisi.py

--- covid19graphs/src/avvisi.py
+++ covid19graphs/src/avvisi.py
@@ -7,9 +7,11 @@ avvisi = open("avvisi.md", "r")
 avviso_odierno = open("../assets/avviso_odierno.txt", "r")
 #Date saved on the previous alert
 data_vecchio_avviso = avviso_odierno.readline().replace("\n", "")
+
 messaggio = ""
 data_ultimo_avviso = ""
 
+#Parsing the latest alert
 for indice, linea in enumerate(avvisi.readlines()):
         if ("## Avvisi" in linea):
             avvisi.seek(0)
@@ -17,12 +19,14 @@ for indice, linea in enumerate(avvisi.readlines()):
             avvisi.seek(0)
             messaggio = avvisi.readlines()[indice + 2].replace("\n", "")
             break
+#Closing avvisi.md after parsing the latest alert
+avvisi.close()
 
+#If the the previous alert date and the new one are different, send a telegram message to the Telegram channel
 if (data_vecchio_avviso != data_ultimo_avviso): 
     avviso_odierno = open("../assets/avviso_odierno.txt", "w")
     avviso_odierno.write(data_ultimo_avviso+"\n"+messaggio)
     msg_formattato = "*"+ data_ultimo_avviso + "*" + "\n" + messaggio
     os.system("telegram-send --format markdown " + "\"" + msg_formattato + "\"")
-    
+#Closing current alert after updating it
 avviso_odierno.close()
-avvisi.close()
