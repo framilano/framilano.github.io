4c8d919217d2a089a9ee25400320996454d9b391
framilano <milano.francesco@protonmail.com>
2021-04-27 15:10:11 +0200

    Added pomorodotimer

---
+++ Projects/pomorodotimer/js/script.js
@@ -0,0 +1,63 @@
+document.getElementById('pomodorobtn').className="selectedbutton"
+document.getElementById('pomodorobtn').addEventListener("click", spawnPomodoroTimer);
+document.getElementById('shortbreakbtn').addEventListener("click", spawnShortBreakTimer);
+document.getElementById('startbtn').addEventListener("click", startTimer);
+
+intervalID = 0
+audio = new Audio('assets/alarm.mp3');
+current_timer = ""
+
+function spawnPomodoroTimer() {
+    document.getElementById('pomodorobtn').className="selectedbutton"
+    document.getElementById('shortbreakbtn').className=""
+    clearInterval(intervalID)
+    current_timer="pomodoro"
+    timer = document.getElementById('timer')
+    timer.value ="pomodoro"
+    timer.innerHTML = "25:00"
+}
+
+function spawnShortBreakTimer() {
+    document.getElementById('shortbreakbtn').className="selectedbutton"
+    document.getElementById('pomodorobtn').className=""
+    clearInterval(intervalID)
+    current_timer = "shortbreak"
+    timer = document.getElementById('timer')
+    timer.value ="shortbreak"
+    timer.innerHTML = "00:10"
+}
+
+function alarm() {
+    document.getElementById('startbtn').innerHTML ="START"
+    clearInterval(intervalID)
+    audio.play();
+    if (current_timer=="pomodoro") document.getElementById('shortbreakbtn').click()
+    else document.getElementById('pomodorobtn').click()
+}
+
+function updateTimer() {
+    res = timer.innerHTML.split(":")
+    minutes = res[0]
+    seconds = res[1]
+    seconds = parseInt(seconds)
+    minutes = parseInt(minutes)
+    seconds = (60 + (seconds - 1)) % 60
+    if (seconds == 59) minutes = minutes - 1
+    if (seconds < 10) seconds = "0" + seconds
+    if (minutes < 10) minutes = "0"+ minutes
+    timer.innerHTML = minutes+":"+seconds
+    if (minutes === "00" && seconds === "00") {
+        alarm()
+    }
+}
+
+function startTimer() {
+    if (document.getElementById('startbtn').innerHTML == "PAUSE") {
+        clearInterval(intervalID)
+        document.getElementById('startbtn').innerHTML ="RESUME"
+    } else {
+        document.getElementById('startbtn').innerHTML ="PAUSE"
+        timer = document.getElementById('timer')
+        intervalID = setInterval(updateTimer, 1000)
+    }
+}
