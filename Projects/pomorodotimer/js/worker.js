5637df9cf3fd06a5bda0a7f95114c1572f402f6a
framilano <milano.francesco@protonmail.com>
2021-10-04 11:54:26 +0200

    Fixed onmessage syntax

--- Projects/pomorodotimer/js/worker.js
+++ Projects/pomorodotimer/js/worker.js
@@ -1,9 +1,9 @@
 var intervalID;
 
-onmessage (comand => {
+onmessage = function(comand) {
     if (comand == "clear") {
         clearInterval(intervalID)
         postMessage()
         return
     } else intervalID = setInterval(postMessage(), 1000)
-});
+};
