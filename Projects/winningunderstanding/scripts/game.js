f6779cdee1195cc740b733c0a221a575cce88a16
Francesco Milano <milano.francesco@protonmail.com>
2023-10-26 10:31:59 +0200

    Added winning understanding buttons

--- Projects/winningunderstanding/scripts/game.js
+++ Projects/winningunderstanding/scripts/game.js
@@ -1,2 +1,59 @@
-var queryString = window.location.search;
-document.getElementById("game-rules").innerHTML = queryString
+var queryString = window.location.search.replace('?', '');
+
+
+var roundLength = queryString.split('&')[0].replace("roundLength=", '')
+var skipAmount = queryString.split('&')[1].replace("skipAmount=", '')
+
+var playButtonElement = document.getElementById("play-button")
+var wordScreen = document.getElementById("word-display")
+var skipButton = document.getElementById("skip-button")
+var pointsCounter = document.getElementById("points-counter")
+var skipCounter = document.getElementById("skip-counter")
+var timeLeft = document.getElementById("time-left")
+var plusButton = document.getElementById("plus-button")
+var minusButton = document.getElementById("minus-button")
+
+var intervalId = 0
+
+timeLeft.innerText = roundLength
+skipCounter.innerText = skipAmount
+
+playButtonElement.addEventListener("click", () => {
+    if (intervalId == 0) {
+        intervalId = startTimer()
+        playButtonElement.innerText = "Pausa"
+    }
+    else {
+        clearInterval(intervalId)
+        playButtonElement.innerText = "Gioca"
+        intervalId = 0
+    }
+})
+
+plusButton.addEventListener("click", () => {
+    scoreInteger = parseInt(pointsCounter.innerText) + 1
+    pointsCounter.innerText = scoreInteger
+})
+minusButton.addEventListener("click", () => {
+    scoreInteger = parseInt(pointsCounter.innerText) - 1
+    if (scoreInteger < 0) return
+    pointsCounter.innerText = scoreInteger
+})
+
+skipButton.addEventListener("click", () => {
+    skipCounterInteger = parseInt(skipCounter.innerText) - 1
+    if (skipCounterInteger < 0) return
+    skipCounter.innerText =  skipCounterInteger
+})
+
+function startTimer() {
+    return setInterval(() => {
+        timeLeft.innerText -= 1
+        if (timeLeft.innerText == 0) {
+            clearInterval(intervalId)
+            playButtonElement.innerText = "Gioca"
+            timeLeft.innerText = queryString.split('&')[0].replace("roundLength=", '')
+            intervalId = 0
+        }
+    }, 1000)
+}
--- Projects/winningunderstanding/scripts/settings.js
+++ Projects/winningunderstanding/scripts/settings.js
@@ -1,6 +1,5 @@
 var roundLength = document.getElementById("round-length-input").value
 var skipAmount = document.getElementById("skip-amount-input").value
-var errorPenalty = document.getElementById("error-penalty-input").checked
 
 document.getElementById("round-length-input").addEventListener("input", () => {
     roundLength = document.getElementById("round-length-input").value
@@ -8,15 +7,10 @@ document.getElementById("round-length-input").addEventListener("input", () => {
 })
 
 document.getElementById("skip-amount-input").addEventListener("input", () => {
-    roundLength = document.getElementById("round-length-input").value
-    document.getElementById("round-length").innerText = roundLength + " secondi"
-})
-
-document.getElementById("error-penalty-input").addEventListener("input", () => {
-    errorPenalty = document.getElementById("error-penalty-input").checked
-    alert(errorPenalty)
+    skipAmount = document.getElementById("skip-amount-input").value
+    document.getElementById("skip-amount").innerText = skipAmount + " passo"
 })
 
 document.getElementById("confirm-button").addEventListener("click", () => {
-    location.href="game.html?roundLength=" + roundLength + "&skipAmount=" + skipAmount + "&errorPenalty=" + errorPenalty
+    location.href="game.html?roundLength=" + roundLength + "&skipAmount=" + skipAmount
 })
